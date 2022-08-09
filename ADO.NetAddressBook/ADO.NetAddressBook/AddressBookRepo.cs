12c54221198452ed760936d9eb55d70d987c5239
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-08-10 02:37:12 +0530

    [Rajesh] Added : Create Database of AddressBook

---
+++ ADO.NetAddressBook/ADO.NetAddressBook/AddressBookRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NetAddressBook
+{
+    public class AddressBookRepo
+    {
+        public void CreateDatabase()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Address_Book_Service;Integrated Security=True;");
+                Connection.Open();
+                SqlCommand command = new SqlCommand("Create Database AddressBook_ADO;", Connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("AddressbookService Database created successfully!");
+                Connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }   
+    }
+}
---
+++ ADO.NetAddressBook/ADO.NetAddressBook/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ADO.NetAddressBook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook ADO.NET Program");
+            Console.WriteLine("1.Create AddrssBookServiceDatabase");
+            int choice = Convert.ToInt16(Console.ReadLine());
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+
+            switch (choice)
+            {
+                case 1:
+                    addressBookRepo.CreateDatabase();
+                    break;
+
+            }
+        }
+    }
+}
